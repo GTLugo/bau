54d4f9fec25a298f2405c4045d0a691b6a2dd31b
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-08-02 21:50:09 -0400

    Update example

--- examples/main.rs
+++ examples/main.rs
@@ -6,19 +6,18 @@ fn main() {
   let s = semaphore.clone();
 
   std::thread::spawn(move || {
-    let _res: std::sync::Arc<i32> = semaphore // Entry
+    let _: i32 = semaphore
       .wait(|| {
-        resource.clone() // Critical section
+        *resource + *resource // Critical section
       })
       .unwrap(); // Call to signal upon exit
   });
 
   std::thread::spawn(move || {
-    {
-      let _guard = s.wait_guard(); // Entry
-      let _res = r;
-      println!("I'm not fuwawa!"); // Critical section
-    } // Call to signal upon drop
+    let _: i32 = {
+      let _guard = s.wait_guard();
+      *r + *r // Critical section
+    }; // Call to signal upon drop
   });
 
   println!("BAU BAU!");
